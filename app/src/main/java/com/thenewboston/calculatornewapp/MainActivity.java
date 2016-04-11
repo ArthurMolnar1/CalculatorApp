321e0fd4c85438ed8b717dee7069050d2cee3a4e
ArthurMolnar1 <molnararthur01@gmail.com>
2016-04-11 23:43:05 +0200

    CalculatorAppTask

---
+++ app/src/main/java/com/thenewboston/calculatornewapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.thenewboston.calculatornewapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View v) {
+
+        Toast.makeText(Toast.LENGTH_LONG).show(int sum);
+
+        EditText e1 = (EditText)findViewById(R.id.editText);
+        EditText e2 = (EditText)findViewById(R.id.editText2);
+        TextView t1 = (TextView)findViewById(R.id.textView);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 + num2;
+        t1.setText(Integer.toString(sum));
+
+    }
+}
